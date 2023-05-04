7314c9c361628e41ca9e76c0bb3806eab665df41
johnwav <102820657+johnwav@users.noreply.github.com>
2023-05-04 01:45:37 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Comment from "./components/comment/Comment";
+
+function App() {
+  return (
+    <div className="app">
+      <Comment />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/comment/Comment.tsx
@@ -0,0 +1,11 @@
+import styles from "./Comment.module.css";
+
+const Comment = () => {
+  return (
+    <>
+      <div className={styles.container}>hey</div>
+    </>
+  );
+};
+
+export default Comment;
