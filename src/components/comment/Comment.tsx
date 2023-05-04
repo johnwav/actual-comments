b83bc40d9e0a7a9ace48f7a1355f1d7258e2b938
johnwav <102820657+johnwav@users.noreply.github.com>
2023-05-04 01:59:04 +0100

    wip: comment box

--- src/components/comment/Comment.tsx
+++ src/components/comment/Comment.tsx
@@ -2,9 +2,13 @@ import styles from "./Comment.module.css";
 
 const Comment = () => {
   return (
-    <>
-      <div className={styles.container}>hey</div>
-    </>
+    <div className={styles.container}>
+      <div className={styles.voteContainer}></div>
+      <div className={styles.commentInfo}>
+        <header></header>
+        <div className={styles.commentBody}></div>
+      </div>
+    </div>
   );
 };
 
